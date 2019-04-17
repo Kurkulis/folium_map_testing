e11d4bcbfb53384b5da2ba66fa30ebca6ea2bac8
Emil Nuutinen <kurkulis@gmail.com>
2019-04-17 11:19:26 +0300

    add marker

--- map.py
+++ map.py
@@ -1,5 +1,14 @@
 import folium
 
+# Create map, centerpoint Kupittaankuja, Turku
 m = folium.Map(location=[60.4472368, 22.2937716], zoom_start=12)
+
+#Global tooltip
+tooltip = 'Click For More'
+
+# Create markers
+folium.Marker([60.4472368, 22.2937716], 
+              popup='My Home',
+              tooltip = tooltip).add_to(m)
 
 m.save('map.html')
