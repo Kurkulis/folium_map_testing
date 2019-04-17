4f6b4ea112e68313280935764d0f609e8fdb3c83
Emil Nuutinen <kurkulis@gmail.com>
2019-04-17 12:17:33 +0300

    add Alicante living area and change coordinates for map and marker

--- map.py
+++ map.py
@@ -1,14 +1,22 @@
 import folium
+import os
 
-# Create map, centerpoint Kupittaankuja, Turku
-m = folium.Map(location=[60.4472368, 22.2937716], zoom_start=12)
+# Create map, centerpoint Alicante, Spain
+m = folium.Map(location=[38.3537, -0.49018499999999676], zoom_start=12)
 
 #Global tooltip
 tooltip = 'Click For More'
 
-# Create markers
-folium.Marker([60.4472368, 22.2937716], 
-              popup='My Home',
-              tooltip = tooltip).add_to(m)
+# Area data JSON
+area = os.path.join('data', 'area.json')
+
+# Create marker pointing to Universidad de Alicante
+folium.Marker([38.3799,-0.5059360000000197], 
+              popup='Universidad de Alicante',
+              tooltip = tooltip,
+              icon = folium.Icon(color='red')).add_to(m)
+
+# Draw area from JSON
+folium.GeoJson(area, name='living area').add_to(m)
 
 m.save('map.html')
